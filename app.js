383ffe6acf78cd267c7debda4dbfa3e76deaa9d4
Ferchus88 <mfernandasanchez88@gmail.com>
2022-04-24 23:30:53 -0300

    login2yCSS

--- app.js
+++ app.js
@@ -18,3 +18,9 @@ app.get('/', (req, res) => {
 app.get('/detalles-del-producto', (req, res) => {
 	res.sendFile(path.resolve('./views/detalles.html'));
 });
+app.get('/Inicio-de-sesion', (req, res) => {
+	res.sendFile(path.resolve('./views/login.html'));
+});
+app.get('/Registro-de-usuario', (req, res) => {
+	res.sendFile(path.resolve('./views/register.html'));
+});
