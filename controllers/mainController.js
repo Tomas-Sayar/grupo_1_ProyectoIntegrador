9c588a3383f6aad47dc8632e3d12c0757cda67b5
Tomas-Sayar <sayartomas@gmail.com>
2022-05-15 15:10:39 -0300

    index.ejs optimizado con partials y forEach para los articles

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,8 +1,16 @@
 const res = require("express/lib/response");
+const fs = require('fs');
+const path = require('path');
+
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
 
 const controller = {
     index: (req, res) => {
-        res.render('index');
+        res.render('index', { products: products });
     },
     login: (req, res) => {
         res.render("login");
--- routers/main.js
+++ routers/main.js
@@ -2,21 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 const mainController = require('../controllers/mainController');
-const productController = require('../controllers/productController');
+
 
 router.get('/', mainController.index);
-
 router.get('/login', mainController.login);
-
 router.get('/register', mainController.register);
-
 router.get('/carrito', mainController.carrito);
-
-<<<<<<< HEAD
-router.get('/product', productController.product);
-=======
 router.get('/productCreate', mainController.productCreate);
->>>>>>> d06980dd60aedc5d48894d69b6acd80d80e91346
+
+
 
 
 module.exports = router;
