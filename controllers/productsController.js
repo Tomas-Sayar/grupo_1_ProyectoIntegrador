384e291feafc088e1cf9acb253f66bb3020ec6a6
Tomas-Sayar <sayartomas@gmail.com>
2022-05-18 21:25:24 -0300

    error -4058 solucionado

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,14 +2,10 @@ const res = require("express/lib/response");
 const fs = require('fs');
 const path = require('path');
 
-
 // ALL PRODUCTS
 const productsFilePath = path.join(__dirname, '../data/ProductsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-// REALATED PRODUCTS
-const relatedProductsFilePath = path.join(__dirname, '../data/relatedProductsDataBase.json');
-const relatedProducts = JSON.parse(fs.readFileSync(relatedProductsFilePath, 'utf-8'));
 
 
 
