877acc9b5d91310c31b52165060dec57373d2589
Tomas-Sayar <sayartomas@gmail.com>
2022-05-22 00:15:34 -0300

    push

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,4 +1,4 @@
-const res = require("express/lib/response");
+const res = require('express/lib/response');
 const fs = require('fs');
 const path = require('path');
 
@@ -6,41 +6,38 @@ const path = require('path');
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-
-
 let controller = {
-    detail: (req, res) => {
-        let id = req.params.id;
-        let product = null;
-        for (let i = 0; i < products.length; i++) {
-            if (id === products[i].id) {
-                product = products[i];
-                break;
-            }
-        }
-        res.render("product-details", { product: product });
-    },
-    create: (req, res) => {
-        res.render("product-create-form");
-    },
-    store: (req, res) => {
-        let newProduct = {
-            id: products.length + 1,
-            name: req.body.name,
-            price: req.body.price,
-            discount: req.body.discount,
-            category: req.body.category,
-            description: req.body.description,
-        }
+	detail: (req, res) => {
+		let id = req.params.id;
+		let product = null;
+		for (let i = 0; i < products.length; i++) {
+			if (id === products[i].id) {
+				product = products[i];
+				break;
+			}
+		}
+		res.render('product-details', { product: product });
+	},
+	create: (req, res) => {
+		res.render('product-create-form');
+	},
+	store: (req, res) => {
+		let newProduct = {
+			id: products.length + 1,
+			name: req.body.name,
+			price: req.body.price,
+			discount: req.body.discount,
+			category: req.body.category,
+			description: req.body.description,
+		};
 
-        products.push(newProduct);
+		products.push(newProduct);
 
-        let productsJSON = JSON.stringify(products);
-        fs.writeFileSync(productsFilePath, productsJSON);
+		let productsJSON = JSON.stringify(products);
+		fs.writeFileSync(productsFilePath, productsJSON);
 
-        res.redirect("/");
-    },
-}
-
+		res.redirect('/');
+	},
+};
 
 module.exports = controller;
