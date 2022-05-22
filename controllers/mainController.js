00f1593d0a76ca5d7031e92c966e44071c8024a2
Tomas-Sayar <sayartomas@gmail.com>
2022-05-22 13:14:05 -0300

    for en mainController.index para que aparezcan solo 8 productos en destacados

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,27 +1,29 @@
-const res = require("express/lib/response");
+const res = require('express/lib/response');
 const fs = require('fs');
 const path = require('path');
-
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-
-
 const controller = {
-    index: (req, res) => {
-        res.render('index', { products: products });
-    },
-    login: (req, res) => {
-        res.render("login");
-    },
-    register: (req, res) => {
-        res.render("register");
-    },
-    carrito: (req, res) => {
-        res.render("carrito");
-    },
+	index: (req, res) => {
+		let featuredProducts = [];
+		for (let i = 0; i < products.length; i++) {
+			if (i < 8) {
+				featuredProducts.push(products[i]);
+			}
+		}
+		res.render('index', { featuredProducts: featuredProducts });
+	},
+	login: (req, res) => {
+		res.render('login');
+	},
+	register: (req, res) => {
+		res.render('register');
+	},
+	carrito: (req, res) => {
+		res.render('carrito');
+	},
 };
-
 
 module.exports = controller;
