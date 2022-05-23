1a10b4fad5ed221586068d6c0425fcaefbb99107
Tomas-Sayar <sayartomas@gmail.com>
2022-05-22 23:26:48 -0300

    push

--- app.js
+++ app.js
@@ -7,19 +7,16 @@ const publicPath = path.resolve(__dirname, './public');
 const mainRouter = require('./routers/main');
 const productsRouter = require('./routers/products');
 
-
-// APP CONFIG 
-app.use(express.static(publicPath));
-app.set('view engine', 'ejs');
+// APP CONFIG
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+app.use(express.static(publicPath));
+app.set('view engine', 'ejs');
 
 // SERVER
 app.listen(3000, () => {
 	console.log('el servidor esta corriendo en el puerto 3000');
 });
-
 
 // ROUTERS
 app.use('/', mainRouter);
